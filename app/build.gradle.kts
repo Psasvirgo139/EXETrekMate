93697edb6b69d50fad0f6f51c1b565379a3d7d8a
Khoi <nguyenngockhoi273@gmail.com>
2026-07-11 02:11:13 +0700

    update version, mark as done PoC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0-mvp"
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
